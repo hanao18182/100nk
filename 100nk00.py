cfffca1d06cbb52319f5a875555f7aa1663c5f2f
hanao18182 <65335251+hanao18182@users.noreply.github.com>
2020-05-21 08:42:35 +0900

    Create 100nk00.py

---
+++ 100nk00.py
@@ -0,0 +1,16 @@
+#最初にUTF-8に設定しておく
+#-*- coding: utf-8 -*-
+
+#stressedを変数に格納する
+s = "stressed"
+
+#逆順を格納する変数を用意する
+gya = ""
+
+#stressedを逆順にする
+gya = s[::-1]
+
+#stressedと逆順にした文字列を出力する
+print(s)
+print(gya)
+
